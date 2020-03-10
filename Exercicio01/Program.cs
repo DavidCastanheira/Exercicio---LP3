2abcb63b4007ba9f01968e5f4ade42497502e9f8
David Castanheira <dvdcastsou8@gmail.com>
2020-03-10 17:50:54 -0300

    Exercicio Pronto

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavras = { "MIAU", "AUAU", "MUUU" };
+            int count = 0;
+
+            foreach (string element in palavras)
+            {
+                count++;
+                Console.WriteLine($"Elemento: {count}: {element}");
+            }
+            Console.WriteLine($"Numbero de elementos: {count}");
+        }
+    }
+}
